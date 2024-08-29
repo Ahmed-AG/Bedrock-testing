9a649d1cfd25abb221318fe65b9f96dddb9c29bc
Ahmad Abu gharbia <aag@Ahmads-MacBook-Pro.local>
2024-08-29 11:53:06 -0700

    Updates

--- basic-test.py
+++ basic-test.py
@@ -3,19 +3,32 @@ import json
 import sys
 
 PROFILE = sys.argv[1]
-PROMPT = sys.argv[2]
+QUERY = sys.argv[2]
 
-session = boto3.Session(profile_name=PROFILE)
+def read_instructions(file_name):
+    with open(file_name, 'r') as file:
+        content = file.read()
+    return str(content)
+
+session = boto3.Session(profile_name=PROFILE, region_name="us-east-1")
 bedrock = session.client('bedrock-runtime')
 
+instructions = read_instructions("spl_instructions.txt")
+# print(instructions)
+
+prompt = instructions + "\n" + QUERY
+
+
 body = {
-        "prompt": PROMPT,
-        # "temperature": 0.5,
-        # "maxTokens": 200,
+        "prompt": prompt,
+        "temperature": 0
+        # "maxTokens": 200
     }
 
 response = bedrock.invoke_model(
-    modelId="cohere.command-light-text-v14",
+    # modelId="cohere.command-light-text-v14",
+    # modelId="amazon.titan-tg1-large",
+    modelId="meta.llama3-8b-instruct-v1:0",
     body=json.dumps(body)
 )
 response_body = json.loads(response.get('body').read())
